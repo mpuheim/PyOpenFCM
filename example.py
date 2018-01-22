4c1971edbff11b55369cba319ce8c6b50c8cb682
mpuheim <puheim@gmail.com>
2018-01-22 16:44:40 +0100

    add new files

--- example.py
+++ example.py
@@ -4,30 +4,37 @@ import json
 print("initialize new FCM with two concepts")
 map=FCM(C1=0.6,C2=0.4)
 print(map)
+input()
 
 print("add third concept")
 map["C3"]=0.5
 print(map)
+input()
 
 print("add fourth concept and connect it to the first")
 map.connect("C1","C4")
 print(map)
+input()
 
 print("make additional connections")
 map.connect("C2","C4")
 map.connect("C3","C4")
 map.connect("C4","C1")
 print(map)
+input()
 
 print("list concepts preceding C4")
 print(map.listPreceding("C4"))
+input()
 
 print("show relation of C4")
 print(map["C4"].relation.get())
+input()
 
 print("set relation of C4 and show the change")
 map["C4"].relation.set("C1",0.2)
 print(map["C4"].relation.get())
+input()
 
 print("FCM in action:")
 print()
